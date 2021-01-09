45c3226c3588ecf1be30147f9e1c4abc094f3446
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-10 00:18:05 +0100

    Monkeypatching - GET - get_all - metoda get_all zwraca Timeout - Red

---
+++ tests/Monkeypatching/test_api_monkeypatching.py
@@ -0,0 +1,16 @@
+import unittest
+
+import requests
+from requests.exceptions import Timeout
+from unittest.mock import Mock, patch
+
+from src.Api import Api
+
+
+class TestApiMonkeyPatch(unittest.TestCase):
+
+    def test_method_api_get_all_raises_timeout(self):
+        with patch('src.Api.Api') as mock_api:
+            with self.assertRaises(Timeout):
+                mock_api.api_get_all()
+
