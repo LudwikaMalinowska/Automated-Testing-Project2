1b2d311bb559e5db287f2462a1b9fe1ad9f7ddaf
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-09 23:49:31 +0100

    Projekt V - testowanie REST API

---
+++ src/Api.py
@@ -0,0 +1,18 @@
+import requests
+
+class Api:
+
+    def api_get_all(self):
+        pass
+
+    def api_get_by_id(self, id):
+        pass
+
+    def api_post(self, data):
+        pass
+
+    def api_put(self, id, data):
+        pass
+
+    def api_delete(self, id):
+        pass
