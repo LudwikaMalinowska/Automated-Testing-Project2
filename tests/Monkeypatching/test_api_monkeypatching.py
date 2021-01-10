f95974d789d83707e1a8237f9ecc277618ec930f
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-11 00:02:18 +0100

    Monkeypatching - POST - api_post - test sprawdzajacy czy metoda api_post zostala wywolana tylko raz - wyjatek

--- tests/Monkeypatching/test_api_monkeypatching.py
+++ tests/Monkeypatching/test_api_monkeypatching.py
@@ -169,6 +169,17 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_api.api_post(mock_data)
             with self.assertRaises(AssertionError):
                 mock_api.api_post.assert_not_called()
+
+    def test_method_api_post_assert_that_called_once_exception(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            mock_data = Mock()
+            mock_data.return_value = {"key": "value"}
+            mock_data2 = Mock()
+            mock_data2.return_value = {"key2": "value2"}
+            mock_api.api_post(mock_data)
+            mock_api.api_post(mock_data2)
+            with self.assertRaises(AssertionError):
+                mock_api.api_post.assert_called_once()
 ##### PUT
     @patch('src.Api.Api', autospec=True)
     def test_method_api_put_raises_timeout(self, mock_class):
