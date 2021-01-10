ecec97b43f24cd9e0f4bc2a1da640e834a57b082
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-10 16:25:16 +0100

    Monkeypatching - GET - api_get_by_id - test sprawdzajacy czy metoda api_get_by_id zostala wywolana tylko raz - Red

--- tests/Monkeypatching/test_api_monkeypatching.py
+++ tests/Monkeypatching/test_api_monkeypatching.py
@@ -28,6 +28,10 @@ class TestApiMonkeyPatch(unittest.TestCase):
         with self.assertRaises(Timeout):
             mock_class.api_get_by_id(mock_id)
 
+    def test_method_api_get_by_id_assert_that_called_once(self):
+        with patch('src.Api.Api') as mock_api:
+            mock_api.api_get_by_id.assert_called_once()
+
     @patch('src.Api.Api')
     def test_method_api_post_raises_timeout(self, mock_class):
         mock_data = Mock()
