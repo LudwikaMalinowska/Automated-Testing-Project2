8195df6f3fc2a97be1c2d1c827ca0c5560a973d8
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-10 22:29:07 +0100

    Monkeypatching - GET - api_get_by_id - test sprawdzajacy czy metoda api_get_by_id zostala wywolana tylko raz i z parametrem id = 1

--- tests/Monkeypatching/test_api_monkeypatching.py
+++ tests/Monkeypatching/test_api_monkeypatching.py
@@ -83,6 +83,13 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_api.api_get_by_id(mock_id)
             mock_api.api_get_by_id.assert_called_with(mock_id)
 
+    def test_method_api_get_by_id_assert_that_called_once_with_id_1(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            mock_id = Mock()
+            mock_id.return_value = 1
+            mock_api.api_get_by_id(mock_id)
+            mock_api.api_get_by_id.assert_called_once_with(mock_id)
+
     def test_method_api_get_by_id_assert_that_not_called_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_id = Mock()
