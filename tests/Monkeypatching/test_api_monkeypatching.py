070830caa53fb98166237d6c7f2f00be31b4e15c
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-10 19:26:30 +0100

    Monkeypatching - Dodanie specyfikacji do wszystkich mockow, parametr autospec=True

--- tests/Monkeypatching/test_api_monkeypatching.py
+++ tests/Monkeypatching/test_api_monkeypatching.py
@@ -10,17 +10,17 @@ from src.Api import Api
 class TestApiMonkeyPatch(unittest.TestCase):
 
     def test_method_api_get_all_raises_timeout(self):
-        with patch('src.Api.Api') as mock_api:
+        with patch('src.Api.Api', autospec=True) as mock_api:
             mock_api.api_get_all.side_effect = Timeout
             with self.assertRaises(Timeout):
                 mock_api.api_get_all()
 
-    @patch('src.Api.Api')
+    @patch('src.Api.Api', autospec=True)
     def test_method_api_get_all_assert_that_called_once(self, mock_class):
         mock_class.api_get_all()
         mock_class.api_get_all.assert_called_once()
 
-    @patch('src.Api.Api')
+    @patch('src.Api.Api', autospec=True)
     def test_method_api_get_by_id_raises_timeout(self, mock_class):
         mock_id = Mock()
         mock_id.return_value = 1
@@ -29,13 +29,13 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_class.api_get_by_id(mock_id)
 
     def test_method_api_get_by_id_assert_that_called_once(self):
-        with patch('src.Api.Api') as mock_api:
+        with patch('src.Api.Api', autospec=True) as mock_api:
             mock_id = Mock()
             mock_id.return_value = 1
             mock_api.api_get_by_id(mock_id)
             mock_api.api_get_by_id.assert_called_once()
 
-    @patch('src.Api.Api')
+    @patch('src.Api.Api', autospec=True)
     def test_method_api_post_raises_timeout(self, mock_class):
         mock_data = Mock()
         mock_data.return_value = {"key": "value"}
@@ -44,13 +44,13 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_class.api_post(mock_data)
 
     def test_method_api_post_assert_that_called_once(self):
-        with patch('src.Api.Api') as mock_api:
+        with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
             mock_data.return_value = {"key": "value"}
             mock_api.api_post(mock_data)
             mock_api.api_post.assert_called_once()
 
-    @patch('src.Api.Api')
+    @patch('src.Api.Api', autospec=True)
     def test_method_api_put_raises_timeout(self, mock_class):
         mock_id = Mock()
         mock_id.return_value = 1
@@ -61,7 +61,7 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_class.api_put(mock_id, mock_data)
 
     def test_method_api_put_assert_that_called_once(self):
-        with patch('src.Api.Api') as mock_api:
+        with patch('src.Api.Api', autospec=True) as mock_api:
             mock_id = Mock()
             mock_id.return_value = 1
             mock_data = Mock()
@@ -69,7 +69,7 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_api.api_put(mock_id, mock_data)
             mock_api.api_put.assert_called_once()
 
-    @patch('src.Api.Api')
+    @patch('src.Api.Api', autospec=True)
     def test_method_api_delete_raises_timeout(self, mock_class):
         mock_id = Mock()
         mock_id.return_value = 1
@@ -78,7 +78,7 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_class.api_delete(mock_id)
 
     def test_method_api_delete_assert_that_called_once(self):
-        with patch('src.Api.Api') as mock_api:
+        with patch('src.Api.Api', autospec=True) as mock_api:
             mock_id = Mock()
             mock_id.return_value = 1
             mock_api.api_delete(mock_id)
