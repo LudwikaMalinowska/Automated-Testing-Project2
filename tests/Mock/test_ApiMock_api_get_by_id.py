732715b562e2b7109daa7fa471d7b790953f497f
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-15 20:17:16 +0100

    Mock - GET - api_get_by_id - test sprawdzajacy czy odpowiedz zwracana przez metode api_get_by_id z parametrem 0 zwroci oczekiwany obiekt: userId: 1, id: 1, title: 'delectus aut autem', completed: false

--- tests/Mock/test_ApiMock_api_get_by_id.py
+++ tests/Mock/test_ApiMock_api_get_by_id.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock, patch
-
+from assertpy import assert_that
 from requests import Timeout
 
 from src.Api import Api
@@ -10,6 +10,20 @@ class TestApi(unittest.TestCase):
 
     def setUp(self):
         self.temp = Api()
+
+    def test_method_api_get_by_id_assert_that_response_equal_to_expected_userId_1_id_1_completed_false(self):
+        self.temp.api_get_by_id = Mock()
+        self.temp.api_get_by_id.status_code = 200
+        self.temp.api_get_by_id.return_value = todos[0]
+        response = self.temp.api_get_by_id(0)
+        first_todo = {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": False
+        }
+
+        assert_that(response).is_equal_to(first_todo)
 
     def tearDown(self):
         self.temp = None
