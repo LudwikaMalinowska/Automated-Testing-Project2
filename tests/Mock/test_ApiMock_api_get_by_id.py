7693684e175a218a829fff2d6584738f203b448a
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 00:34:44 +0100

    Mock - GET - api_get_by_id - test_method_api_get_by_id_assert_that_response_contains_key_completed - test sprawdzajacy czy odpowiedz zwracana przez metode_api_get_by_id z parametrem 0 posiada klucz completed - poprawki

--- tests/Mock/test_ApiMock_api_get_by_id.py
+++ tests/Mock/test_ApiMock_api_get_by_id.py
@@ -47,10 +47,10 @@ class TestApi(unittest.TestCase):
 
     def test_method_api_get_by_id_assert_that_response_contains_key_completed(self):
         self.temp.api_get_by_id = Mock()
-        self.temp.api_get_by_id.return_value = todos[0]
+        self.temp.api_get_by_id.return_value = {"data": todos[0], "status_code": 200}
         response = self.temp.api_get_by_id(0)
 
-        assert_that(response).contains_key("completed")
+        assert_that(response["data"]).contains_key("completed")
 
     def test_method_api_get_by_id_assert_that_response_contains_all_the_keys_userId_id_title_completed(self):
         self.temp.api_get_by_id = Mock()
