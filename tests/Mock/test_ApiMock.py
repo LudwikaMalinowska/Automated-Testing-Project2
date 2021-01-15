7d826afca840d56d39d865c60f9be89c34a01bee
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-15 14:19:29 +0100

    Mock - GET - api_get_all - test sprawdzajacy czy odpowiedz zwracana przez metode api_get_all zgadza sie z oczekiwana odpowiedza (todo)

--- tests/Mock/test_ApiMock.py
+++ tests/Mock/test_ApiMock.py
@@ -15,6 +15,13 @@ class TestApi(unittest.TestCase):
         result = self.temp.api_get_all()
         self.assertEqual(len(result), 200)
 
+    def test_method_api_get_all_assert_that_result_equal_todos(self):
+        self.temp.api_get_all = Mock()
+        self.temp.api_get_all.return_value = todos
+
+        result = self.temp.api_get_all()
+        self.assertEqual(result, todos)
+
 
     def tearDown(self):
         self.temp = None
