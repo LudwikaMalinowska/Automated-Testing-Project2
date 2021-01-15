63e2a687f8b8241a9b685a006eaefb8f8727c1da
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-15 23:51:21 +0100

    Mock - GET - api_get_all - test_method_api_get_all_assert_that_list_of_keys_equal_to_expected_list_of_keys - test sprawdzajacy czy lista kluczy pierwszego rekordu w zwracanym przez metode api_get_all wyniku jest zgodna z oczekiwana lista kluczy: 'userId', 'id', 'title', 'completed' - poprawki

--- tests/Mock/test_ApiMock_api_get_all.py
+++ tests/Mock/test_ApiMock_api_get_all.py
@@ -41,10 +41,10 @@ class TestApi(unittest.TestCase):
 
     def test_method_api_get_all_assert_that_list_of_keys_equal_to_expected_list_of_keys(self):
         self.temp.api_get_all = Mock()
-        self.temp.api_get_all.return_value = todos
+        self.temp.api_get_all.return_value = {"data": todos, "status_code": 200}
         result = self.temp.api_get_all()
 
-        mock_keys = list(result[0].keys())
+        mock_keys = list(result["data"][0].keys())
         expected_keys = ["userId", "id", "title", "completed"]
 
         self.assertListEqual(mock_keys, expected_keys)
