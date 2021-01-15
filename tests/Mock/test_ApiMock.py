0d9dda1bc24d7c665264a1fc94fe1b35eeeefcea
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-15 14:03:44 +0100

    Mock - GET - api_get_all - test sprawdzajacy czy odpowiedz zwrocona przez metode api_get_all ma dlugosc 200, czy odpowiedz zawiera 200 obiektow

--- tests/Mock/test_ApiMock.py
+++ tests/Mock/test_ApiMock.py
@@ -1,13 +1,19 @@
 import unittest
 from unittest.mock import Mock, patch
 from src.Api import Api
+from tests.Mock.todos import todos
 
 class TestApi(unittest.TestCase):
 
     def setUp(self):
         self.temp = Api()
 
-    
+    def test_method_api_get_all_assert_that_result_length_equal_200(self):
+        self.temp.api_get_all = Mock()
+        self.temp.api_get_all.return_value = todos
+
+        result = self.temp.api_get_all()
+        self.assertEqual(len(result), 200)
 
 
     def tearDown(self):
