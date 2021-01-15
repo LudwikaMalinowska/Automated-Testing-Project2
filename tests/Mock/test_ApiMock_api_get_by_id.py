4c875db5fbdc091a2f2a3fede75781d492a9c70c
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 00:32:19 +0100

    Mock - GET - api_get_by_id - test_method_api_get_by_id_assert_that_response_contains_key_title - test sprawdzajacy czy odpowiedz zwracana przez metode_api_get_by_id z parametrem 0 posiada klucz title - poprawki

--- tests/Mock/test_ApiMock_api_get_by_id.py
+++ tests/Mock/test_ApiMock_api_get_by_id.py
@@ -40,10 +40,10 @@ class TestApi(unittest.TestCase):
 
     def test_method_api_get_by_id_assert_that_response_contains_key_title(self):
         self.temp.api_get_by_id = Mock()
-        self.temp.api_get_by_id.return_value = todos[0]
+        self.temp.api_get_by_id.return_value = {"data": todos[0], "status_code": 200}
         response = self.temp.api_get_by_id(0)
 
-        assert_that(response).contains_key("title")
+        assert_that(response["data"]).contains_key("title")
 
     def test_method_api_get_by_id_assert_that_response_contains_key_completed(self):
         self.temp.api_get_by_id = Mock()
