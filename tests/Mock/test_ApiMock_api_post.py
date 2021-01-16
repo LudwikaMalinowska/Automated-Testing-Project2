34ba26a4855f17ed0c0d3b068acc6b86a3d37d5e
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 15:50:26 +0100

    Mock - POST - api_post - test sprawdzajacy czy metoda api_post zwraca status code 200

--- tests/Mock/test_ApiMock_api_post.py
+++ tests/Mock/test_ApiMock_api_post.py
@@ -12,6 +12,19 @@ class TestApi(unittest.TestCase):
     def setUp(self):
         self.temp = Api()
 
+    def test_method_api_post_assert_that_response_has_status_code_200(self):
+        self.temp.api_post = Mock()
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_post.return_value = {"posted_data": todo, "status_code": 200}
+
+        response = self.temp.api_post(todo)
+
+        assert_that(response["status_code"]).is_equal_to(200)
+
     def tearDown(self):
         self.temp = None
 
