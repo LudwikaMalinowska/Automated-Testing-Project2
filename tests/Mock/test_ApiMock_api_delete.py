f0bfdad031fc5d2433f7187ee63eb235bcfb2492
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 21:42:20 +0100

    Mock - DELETE - api_delete - test sprawdzajacy czy metoda api_delete zwraca status code rozny od 200

--- tests/Mock/test_ApiMock_api_delete.py
+++ tests/Mock/test_ApiMock_api_delete.py
@@ -21,6 +21,14 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["status_code"]).is_equal_to(200)
 
+    def test_method_api_delete_assert_that_response_status_code_is_not_200(self):
+        self.temp.api_delete = Mock()
+        todo_id = 1
+        self.temp.api_delete.return_value = {"status_code": 408}
+        response = self.temp.api_delete(todo_id)
+
+        assert_that(response["status_code"]).is_not_equal_to(200)
+
 
     def tearDown(self):
         self.temp = None
