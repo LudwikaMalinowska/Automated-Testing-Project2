b4fb36fe366de3f9a787524b86160bfcea7b7a06
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 17:17:17 +0100

    Mock - POST - api_post - test sprawdzajacy czy metoda api_post zwraca zapostowane dane

--- tests/Mock/test_ApiMock_api_post.py
+++ tests/Mock/test_ApiMock_api_post.py
@@ -25,6 +25,18 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["status_code"]).is_equal_to(200)
 
+    def test_method_api_post_assert_that_response_returns_posted_data(self):
+        self.temp.api_post = Mock()
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_post.return_value = {"posted_data": todo, "status_code": 200}
+        response = self.temp.api_post(todo)
+
+        assert_that(response["posted_data"]).is_equal_to(todo)
+
     def test_method_api_post_assert_that_response_status_code_is_not_200_exception(self):
         self.temp.api_post = Mock()
         todo = {
@@ -34,7 +46,7 @@ class TestApi(unittest.TestCase):
         }
         self.temp.api_post.return_value = {"status_code": 408}
         response = self.temp.api_post(todo)
-        
+
         assert_that(response["status_code"]).is_not_equal_to(200)
 
     def test_method_api_post_assert_that_response_returns_Timeout_exception(self):
