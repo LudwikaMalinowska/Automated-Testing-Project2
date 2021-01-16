6862100dbb22aa1d5915732eb1b009d27b31ffba
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 15:57:30 +0100

    Mock - POST - api_post - test sprawdzajacy czy metoda api_post zwraca Timeout - wyjatek

--- tests/Mock/test_ApiMock_api_post.py
+++ tests/Mock/test_ApiMock_api_post.py
@@ -25,6 +25,18 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["status_code"]).is_equal_to(200)
 
+    def test_method_api_post_assert_that_response_returns_Timeout_exception(self):
+        self.temp.api_post = Mock()
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_post.return_value = {"status_code": 408}
+        self.temp.api_post.side_effect = Timeout
+
+        assert_that(self.temp.api_post).raises(Timeout).when_called_with(todo)
+
     def tearDown(self):
         self.temp = None
 
