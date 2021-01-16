39d89d42f6a8797b476f8e8b1438def0c1f09493
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 21:12:09 +0100

    Mock - PUT - api_put - test sprawdzajacy ze odpowiedz zwracana przez metode api_put posiada klucz put_id o wartosci 1

--- tests/Mock/test_ApiMock_api_put.py
+++ tests/Mock/test_ApiMock_api_put.py
@@ -179,6 +179,21 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["put_data"]).has_completed(False)
 
+    def test_method_api_put_assert_that_response_has_key_put_id_1(self):
+        self.temp.api_put = Mock()
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put.return_value = {"put_id": todo_id,
+                                          "put_data": todo, "status_code": 200}
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response).has_put_id(1)
+
+
     def test_method_api_put_assert_that_response_returns_Timeout_exception(self):
         self.temp.api_put = Mock()
         todo_id = 1
@@ -189,7 +204,7 @@ class TestApi(unittest.TestCase):
         }
         self.temp.api_put.return_value = {"status_code": 408}
         self.temp.api_put.side_effect = Timeout
-        
+
         assert_that(self.temp.api_put).raises(Timeout).when_called_with(todo_id, todo)
 
 
