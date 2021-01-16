5c4f8d29a92d73d40ee76bc19480f9326bef12b6
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 23:07:27 +0100

    Mock - DELETE - api_delete - test sprawdzajacy ze dane zwracane przez metode api_delete posiadaja klucz userId

--- tests/Mock/test_ApiMock_api_delete.py
+++ tests/Mock/test_ApiMock_api_delete.py
@@ -60,6 +60,16 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["deleted_data"]).is_equal_to(todos[1])
 
+    def test_method_api_delete_assert_that_response_deleted_data_contain_key_userId(self):
+        self.temp.api_delete = Mock()
+        todo_id = 1
+        self.temp.api_delete.return_value = {"delete_id": todo_id,
+                                             "deleted_data": todos[todo_id],
+                                             "status_code": 200}
+        response = self.temp.api_delete(todo_id)
+
+        assert_that(response["deleted_data"]).contains_key("userId")
+
 
 
     def test_method_api_delete_assert_that_response_returns_Timeout_exception(self):
