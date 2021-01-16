033641aa55da00b234d8d77fc6d75df25f57c3d1
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 00:48:10 +0100

    Mock - PUT - api_put - test sprawdzajacy czy metoda api_put zwraca AttributeError gdy zostala wywolana z id=None - wyjatek

--- tests/Mock/test_ApiMock_api_put.py
+++ tests/Mock/test_ApiMock_api_put.py
@@ -232,7 +232,7 @@ class TestApi(unittest.TestCase):
 
         assert_that(self.temp.api_put).raises(ValueError).when_called_with(todo_id, todo)
 
-    def test_method_api_put_assert_that_response_returns_ValueError_when_called_with_id_not_int_exception(self):
+    def test_method_api_put_assert_that_response_returns_TypeError_when_called_with_id_not_int_exception(self):
         self.temp.api_put = Mock()
         todo_id = "1"
         todo = {
@@ -244,6 +244,19 @@ class TestApi(unittest.TestCase):
         self.temp.api_put.side_effect = TypeError
 
         assert_that(self.temp.api_put).raises(TypeError).when_called_with(todo_id, todo)
+
+    def test_method_api_put_assert_that_response_returns_AttributeError_when_called_with_id_None_exception(self):
+        self.temp.api_put = Mock()
+        todo_id = None
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put.return_value = {"status_code": 408}
+        self.temp.api_put.side_effect = AttributeError
+
+        assert_that(self.temp.api_put).raises(AttributeError).when_called_with(todo_id, todo)
 
 
 
