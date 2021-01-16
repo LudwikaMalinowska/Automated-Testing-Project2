281533c17b3cb938d49b0f5df6c13d009b751872
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 19:47:26 +0100

    Mock - PUT - api_put - test sprawdzajacy ze dane zwracane przez metode api_put posiadaja klucze: userId, title, completed

--- tests/Mock/test_ApiMock_api_put.py
+++ tests/Mock/test_ApiMock_api_put.py
@@ -109,6 +109,20 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["put_data"]).contains_key("completed")
 
+    def test_method_api_post_assert_that_response_put_data_contain_all_keys_userId_title_completed(self):
+        self.temp.api_put = Mock()
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put.return_value = {"put_id": todo_id,
+                                          "put_data": todo, "status_code": 200}
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response["put_data"]).contains_key("userId", "title", "completed")
+
 
 
     def tearDown(self):
