1bea8758fdc884f2339cad48847c45850fc542b8
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 00:43:01 +0100

    Mock - PUT - api_put - test sprawdzajacy czy metoda api_put zwraca ValueError gdy zostala wywolana z id=300 (id spoza tablicy) - wyjatek

--- tests/Mock/test_ApiMock_api_put.py
+++ tests/Mock/test_ApiMock_api_put.py
@@ -219,6 +219,19 @@ class TestApi(unittest.TestCase):
 
         assert_that(self.temp.api_put).raises(ValueError).when_called_with(todo_id, todo)
 
+    def test_method_api_put_assert_that_response_returns_ValueError_when_called_with_id_300_exception(self):
+        self.temp.api_put = Mock()
+        todo_id = 300
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put.return_value = {"status_code": 408}
+        self.temp.api_put.side_effect = ValueError
+
+        assert_that(self.temp.api_put).raises(ValueError).when_called_with(todo_id, todo)
+
 
 
     def tearDown(self):
