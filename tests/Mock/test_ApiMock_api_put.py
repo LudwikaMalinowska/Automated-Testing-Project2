8958c4b40012b747244b198f403a2f56c04d9eb4
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-16 18:55:01 +0100

    Mock - PUT - api_put - test sprawdzajacy czy metoda api_put zwraca status code 200

--- tests/Mock/test_ApiMock_api_put.py
+++ tests/Mock/test_ApiMock_api_put.py
@@ -12,6 +12,20 @@ class TestApi(unittest.TestCase):
     def setUp(self):
         self.temp = Api()
 
+    def test_method_api_put_assert_that_response_has_status_code_200(self):
+        self.temp.api_post = Mock()
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_post.return_value = {"put_id": todo_id,
+                                           "put_data": todo, "status_code": 200}
+        response = self.temp.api_post(todo)
+
+        assert_that(response["status_code"]).is_equal_to(200)
+
     def tearDown(self):
         self.temp = None
 
