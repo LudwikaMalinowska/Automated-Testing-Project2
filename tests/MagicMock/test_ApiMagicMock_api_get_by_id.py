b3ff074e0150d8e801fdd409ec46176899c703e3
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 11:06:28 +0100

    MagicMock - GET - api_get_by_id - test sprawdzajacy ze metoda api_get_by_id zwraca oczekiwane dane (oczekiwany obiekt todo): z userId: 1, id: 1, title: 'delectus aut autem', completed: False

--- tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
+++ tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock, patch
 from assertpy import assert_that
 from requests import Timeout
 
@@ -8,7 +8,25 @@ from tests.Mock.todos import todos
 
 
 class TestApi(unittest.TestCase):
-    ...
+
+    def setUp(self):
+        self.temp = Api()
+
+    def test_method_api_get_by_id_assert_that_response_equal_to_expected_userId_1_id_1_completed_false(self):
+        todo_id = 1
+        self.temp.api_get_by_id = MagicMock(return_value={"data": todos[todo_id - 1], "status_code": 200})
+        response = self.temp.api_get_by_id(todo_id)
+        expected_todo = {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": False
+          }
+
+        assert_that(response["data"]).is_equal_to(expected_todo)
+
+    def tearDown(self):
+        self.temp = None
 
 
 if __name__ == '__main__':
