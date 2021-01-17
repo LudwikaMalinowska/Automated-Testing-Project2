01bf3bdf4470b4e646ffb81c95ac820f4eefdb3b
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 11:28:52 +0100

    MagicMock - GET - api_get_by_id - test sprawdzajacy ze dane zwracane przez metode api_get_by_id posiadaja klucz title

--- tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
+++ tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
@@ -39,6 +39,13 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["data"]).contains_key("id")
 
+    def test_method_api_get_by_id_assert_that_response_contains_key_title(self):
+        todo_id = 1
+        self.temp.api_get_by_id = MagicMock(return_value={"data": todos[todo_id - 1], "status_code": 200})
+        response = self.temp.api_get_by_id(todo_id)
+
+        assert_that(response["data"]).contains_key("title")
+
     def tearDown(self):
         self.temp = None
 
