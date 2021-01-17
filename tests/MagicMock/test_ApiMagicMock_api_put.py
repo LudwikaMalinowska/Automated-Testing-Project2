152351812427d16c3fcf199f61de78f3e8c92a53
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 14:48:02 +0100

    MagicMock - PUT - api_put - test sprawdzajacy ze dane zwracane przez metode api_put posiadaja klucz title o wartosci 'Lorem'

--- tests/MagicMock/test_ApiMagicMock_api_put.py
+++ tests/MagicMock/test_ApiMagicMock_api_put.py
@@ -128,6 +128,19 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["put_data"]).has_userId(1)
 
+    def test_method_api_put_assert_that_response_put_data_has_key_title_Lorem(self):
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put = MagicMock(return_value={"put_id": todo_id,
+                                                    "put_data": todo, "status_code": 200})
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response["put_data"]).has_title("Lorem")
+
     def tearDown(self):
         self.temp = None
 
