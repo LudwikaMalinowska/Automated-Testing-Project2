90645c60e49b95988b13bd7da21170dc1d686343
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 20:54:57 +0100

    Monkeypatching - POST - api_post - test sprawdzajacy czy metoda api_post zwraca zapostowane dane

--- tests/Monkeypatching/test_Api_monkeypatching_api_post.py
+++ tests/Monkeypatching/test_Api_monkeypatching_api_post.py
@@ -105,6 +105,17 @@ class TestApiMonkeyPatch(unittest.TestCase):
             response = mock_api.api_post(post_todo)
             assert_that(response["status_code"]).is_not_equal_to(200)
 
+    def test_method_api_post_assert_that_response_returns_posted_data(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            post_todo = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
+            mock_api.api_post.return_value = {"posted_data": post_todo, "status_code": 200}
+            response = mock_api.api_post(post_todo)
+            assert_that(response["posted_data"]).is_equal_to(post_todo)
+
     def test_method_api_post_assert_that_not_called_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
