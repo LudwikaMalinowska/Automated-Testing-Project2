b45e66c3e9350836262d5c352817b5012779a039
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 16:59:11 +0100

    MagicMock - DELETE - api_delete - test sprawdzajacy ze dane zwracane przez metode api_delete posiadaja klucz userId o wartosci 1

--- tests/MagicMock/test_ApiMagicMock_api_delete.py
+++ tests/MagicMock/test_ApiMagicMock_api_delete.py
@@ -83,6 +83,13 @@ class TestApi(unittest.TestCase):
         response = self.temp.api_delete(todo_id)
         assert_that(response["deleted_data"]).contains_key("userId", "id", "title", "completed")
 
+    def test_method_api_delete_assert_that_response_deleted_data_has_key_userId_and_its_1(self):
+        todo_id = 1
+        self.temp.api_delete = MagicMock(return_value={"delete_id": todo_id, "deleted_data": todos[todo_id - 1],
+                                                       "status_code": 200})
+        response = self.temp.api_delete(todo_id)
+        assert_that(response["deleted_data"]).has_userId(1)
+
     def tearDown(self):
         self.temp = None
 
