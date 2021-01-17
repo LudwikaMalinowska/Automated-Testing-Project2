95e79f0256e97d858548ad445211d1985a43ff9a
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 15:35:40 +0100

    MagicMock - DELETE - api_delete - test sprawdzajacy ze odpowiedz zwracana przez metode api_delete ma status code 200

--- tests/MagicMock/test_ApiMagicMock_api_delete.py
+++ tests/MagicMock/test_ApiMagicMock_api_delete.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock, patch
 from assertpy import assert_that
 from requests import Timeout
 
@@ -8,7 +8,20 @@ from tests.Mock.todos import todos
 
 
 class TestApi(unittest.TestCase):
-    ...
+
+    def setUp(self):
+        self.temp = Api()
+
+    def test_method_api_delete_assert_that_response_has_status_code_200(self):
+        todo_id = 1
+        self.temp.api_delete = MagicMock(return_value={"delete_id": todo_id, "deleted_data": todos[todo_id - 1],
+                                                       "status_code": 200})
+        response = self.temp.api_delete(todo_id)
+
+        assert_that(response).has_status_code(200)
+
+    def tearDown(self):
+        self.temp = None
 
 
 if __name__ == '__main__':
