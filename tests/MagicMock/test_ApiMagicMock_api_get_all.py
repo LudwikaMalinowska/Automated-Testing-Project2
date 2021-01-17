5613a67e6ff0f0ac45aaa6cb4c56e9c6b8a47583
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 02:21:34 +0100

    MagicMock - GET - api_get_all - test sprawdzajacy czy pierwszy rekord odpowiedzi jest zgodny z oczekiwanym tzn userID: 1, id: 1, title: delectus aut autem, completed: False

--- tests/MagicMock/test_ApiMagicMock_api_get_all.py
+++ tests/MagicMock/test_ApiMagicMock_api_get_all.py
@@ -24,6 +24,12 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["data"]).is_equal_to(todos)
 
+    def test_method_api_get_all_assert_that_1st_record_of_response_equal_expected_record(self):
+        self.temp.api_get_all = MagicMock(return_value={"data": todos, "status_code": 200})
+        response = self.temp.api_get_all()
+
+        assert_that(response["data"][0]).is_equal_to(todos[0])
+
     def tearDown(self):
         self.temp = None
 
