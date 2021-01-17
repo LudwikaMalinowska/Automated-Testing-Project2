fd3889fa96ec6a3d941e06530f04835c5c3391fc
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 22:15:05 +0100

    Monkeypatching - DELETE - api_delete - test sprawdzajacy czy dane zwracane przez metode api_delete posiadaja klucze: userId, id, title, completed

--- tests/Monkeypatching/test_Api_monkeypatching_api_delete.py
+++ tests/Monkeypatching/test_Api_monkeypatching_api_delete.py
@@ -103,6 +103,16 @@ class TestApiMonkeyPatch(unittest.TestCase):
             response = mock_api.api_delete(todo_id)
     
             assert_that(response["deleted_data"]).is_equal_to(todos[0])
+    
+    def test_method_api_delete_assert_that_response_deleted_data_contain_all_keys_userId_id_title_completed(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            todo_id = 1
+            mock_api.api_delete.return_value = {"delete_id": todo_id,
+                                                "deleted_data": todos[todo_id - 1],
+                                                "status_code": 200}
+            response = mock_api.api_delete(todo_id)
+    
+            assert_that(response["deleted_data"]).contains_key("userId", "id", "title", "completed")
 
     def test_method_api_delete_assert_that_not_called_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
