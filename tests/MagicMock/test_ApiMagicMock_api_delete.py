5f6a6c70d5cf4a8b64bcc772fac2a52d319a03a5
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 17:03:33 +0100

    MagicMock - DELETE - api_delete - test sprawdzajacy ze dane zwracane przez metode api_delete posiadaja klucz completed o wartosci False

--- tests/MagicMock/test_ApiMagicMock_api_delete.py
+++ tests/MagicMock/test_ApiMagicMock_api_delete.py
@@ -104,6 +104,13 @@ class TestApi(unittest.TestCase):
         response = self.temp.api_delete(todo_id)
         assert_that(response["deleted_data"]).has_title("delectus aut autem")
 
+    def test_method_api_delete_assert_that_response_deleted_data_has_key_completed_and_its_False(self):
+        todo_id = 1
+        self.temp.api_delete = MagicMock(return_value={"delete_id": todo_id, "deleted_data": todos[todo_id - 1],
+                                                       "status_code": 200})
+        response = self.temp.api_delete(todo_id)
+        assert_that(response["deleted_data"]).has_completed(False)
+
     def tearDown(self):
         self.temp = None
 
