0d55e9589847f7ce0e5567b41046287baffcb324
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 01:18:44 +0100

    Monkeypatching - reorganizacja plikow z testami monkeypatching - utworzenie pliku na testy dla metody api_get_by_id

---
+++ tests/Monkeypatching/test_Api_monkeypatching_api_get_by_id.py
@@ -0,0 +1,15 @@
+import unittest
+
+import requests
+from requests.exceptions import Timeout
+from unittest.mock import Mock, patch
+
+from src.Api import Api
+
+
+class TestApiMonkeyPatch(unittest.TestCase):
+    ...
+
+
+if __name__ == '__main__':
+    unittest.main()
