3845919c6781c9ba61fc3e19377f34bf25d0b618
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 21:58:42 +0100

    Monkeypatching - PUT - api_put - test sprawdzajacy ze metoda api_put zwraca ValueError gdy zostanie wywolana z obiektem bez klucza userId - wyjatek

--- tests/Monkeypatching/test_Api_monkeypatching_api_put.py
+++ tests/Monkeypatching/test_Api_monkeypatching_api_put.py
@@ -315,6 +315,18 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_api.api_put.side_effect = ValueError
     
             assert_that(mock_api.api_put).raises(ValueError).when_called_with(todo_id, todo)
+    
+    def test_method_api_put_assert_that_response_returns_ValueError_when_called_with_obj_without_key_userId_exception(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            todo_id = 1
+            todo = {
+                "title": "Lorem",
+                "completed": False
+            }
+            mock_api.api_put.return_value = {"status_code": 408}
+            mock_api.api_put.side_effect = ValueError
+    
+            assert_that(mock_api.api_put).raises(ValueError).when_called_with(todo_id, todo)
 
 
 if __name__ == '__main__':
