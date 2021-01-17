faa6c8d821c3fc96be5f168c1a001fbfc92fba70
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 14:45:07 +0100

    MagicMock - PUT - api_put - test sprawdzajacy ze dane zwracane przez metode api_put posiadaja klucz completed

--- tests/MagicMock/test_ApiMagicMock_api_put.py
+++ tests/MagicMock/test_ApiMagicMock_api_put.py
@@ -89,6 +89,19 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["put_data"]).contains_key("title")
 
+    def test_method_api_put_assert_that_response_put_data_contain_key_completed(self):
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put = MagicMock(return_value={"put_id": todo_id,
+                                                    "put_data": todo, "status_code": 200})
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response["put_data"]).contains_key("completed")
+
     def tearDown(self):
         self.temp = None
 
