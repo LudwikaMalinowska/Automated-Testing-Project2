d52ff4d67c1761babdc8c82430433617ff4297f9
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 14:20:47 +0100

    MagicMock - PUT - api_put - test sprawdzajacy ze odpowiedz zwracana przez metode api_put ma status code 200

--- tests/MagicMock/test_ApiMagicMock_api_put.py
+++ tests/MagicMock/test_ApiMagicMock_api_put.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock, patch
 from assertpy import assert_that
 from requests import Timeout
 
@@ -8,7 +8,24 @@ from tests.Mock.todos import todos
 
 
 class TestApi(unittest.TestCase):
-    ...
+
+    def setUp(self):
+        self.temp = Api()
+
+    def test_method_api_put_assert_that_response_has_status_code_200(self):
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put = MagicMock(return_value={"put_data": todo, "status_code": 200})
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response).has_status_code(200)
+
+    def tearDown(self):
+        self.temp = None
 
 
 if __name__ == '__main__':
