89bd93f69441f64c9a831a057131ed86bf0b932a
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 01:35:59 +0100

    MagicMock - utworzenie pliku na testy MagicMock dla metody api_get_by_id

---
+++ tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import Mock, patch
+from assertpy import assert_that
+from requests import Timeout
+
+from src.Api import Api
+from tests.Mock.todos import todos
+
+
+class TestApi(unittest.TestCase):
+    ...
+
+
+if __name__ == '__main__':
+    unittest.main()
