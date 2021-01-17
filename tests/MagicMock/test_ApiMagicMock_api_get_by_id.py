61271717fc3b871172bd5c81dd78ed0d6b186278
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 12:33:13 +0100

    MagicMock - GET - api_get_by_id - test sprawdzajacy ze metoda api_get_by_id zwraca AttributeError przy wywolaniu z parametrem id=None - wyjatek

--- tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
+++ tests/MagicMock/test_ApiMagicMock_api_get_by_id.py
@@ -121,6 +121,12 @@ class TestApi(unittest.TestCase):
 
         assert_that(self.temp.api_get_by_id).raises(TypeError).when_called_with(todo_id)
 
+    def test_method_api_bet_by_id_assert_that_response_returns_AttributeError_when_called_with_id_None_exception(self):
+        todo_id = None
+        self.temp.api_get_by_id = MagicMock(return_value={"status_code": 408}, side_effect=AttributeError)
+
+        assert_that(self.temp.api_get_by_id).raises(AttributeError).when_called_with(todo_id)
+
     def tearDown(self):
         self.temp = None
 
