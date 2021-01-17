b3fc973a232e8309de5a669827306024975ca069
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 16:27:31 +0100

    MagicMock - DELETE - api_delete - test sprawdzajacy ze odpowiedz zwracana przez metode api_delete jest typu dict (jest slownikiem)

--- tests/MagicMock/test_ApiMagicMock_api_delete.py
+++ tests/MagicMock/test_ApiMagicMock_api_delete.py
@@ -26,6 +26,14 @@ class TestApi(unittest.TestCase):
         response = self.temp.api_delete(todo_id)
         assert_that(response["status_code"]).is_not_equal_to(200)
 
+    def test_method_api_delete_assert_that_response_is_instance_of_dict(self):
+        todo_id = 1
+        self.temp.api_delete = MagicMock(return_value={"delete_id": todo_id, "deleted_data": todos[todo_id - 1],
+                                                       "status_code": 200})
+        response = self.temp.api_delete(todo_id)
+
+        assert_that(response).is_instance_of(dict)
+
     def tearDown(self):
         self.temp = None
 
