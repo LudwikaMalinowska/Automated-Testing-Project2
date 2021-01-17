36cbbfcabeba5a18f9ff11b567192b19639c0025
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 17:13:00 +0100

    MagicMock - DELETE - api_delete - test sprawdzajacy ze metoda api_delete zwraca ValueError przy wyolaniu z parametrem id=300 - wyjatek

--- tests/MagicMock/test_ApiMagicMock_api_delete.py
+++ tests/MagicMock/test_ApiMagicMock_api_delete.py
@@ -123,6 +123,12 @@ class TestApi(unittest.TestCase):
 
         assert_that(self.temp.api_delete).raises(ValueError).when_called_with(todo_id)
 
+    def test_method_api_delete_assert_that_response_returns_ValueError_when_called_with_id_300_exception(self):
+        todo_id = 300
+        self.temp.api_delete = MagicMock(return_value={"status_code": 408}, side_effect=ValueError)
+
+        assert_that(self.temp.api_delete).raises(ValueError).when_called_with(todo_id)
+
 
     def tearDown(self):
         self.temp = None
