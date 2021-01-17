c1309b8f72881d260cb0b523f708d373ff038315
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 14:28:00 +0100

    MagicMock - PUT - api_put - test sprawdzajacy ze dane zwracane przez metode api_put nie zawieraja klucza id

--- tests/MagicMock/test_ApiMagicMock_api_put.py
+++ tests/MagicMock/test_ApiMagicMock_api_put.py
@@ -36,6 +36,18 @@ class TestApi(unittest.TestCase):
 
         assert_that(response["put_data"]).is_equal_to(todo)
 
+    def test_method_api_post_assert_that_response_returns_posted_data_doesnt_contain_key_id(self):
+        todo_id = 1
+        todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_put = MagicMock(return_value={"put_data": todo, "status_code": 200})
+        response = self.temp.api_put(todo_id, todo)
+
+        assert_that(response["put_data"]).does_not_contain_key("id")
+
     def tearDown(self):
         self.temp = None
 
