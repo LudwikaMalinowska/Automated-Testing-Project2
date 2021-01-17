9783f89008952cd9aa61abe3747e08a9510b0f9a
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 14:14:53 +0100

    MagicMock - POST - api_post - test sprawdzajacy ze metoda api_post zwraca ValueError przy wywolaniu z obiektem bez pola completed

--- tests/MagicMock/test_ApiMagicMock_api_post.py
+++ tests/MagicMock/test_ApiMagicMock_api_post.py
@@ -180,6 +180,16 @@ class TestApi(unittest.TestCase):
 
         assert_that(self.temp.api_post).raises(ValueError).when_called_with(post_todo)
 
+    def test_method_api_post_assert_that_response_returns_ValueError_when_called_with_obj_without_key_completed_exception(
+            self):
+        post_todo = {
+            "userId": 1,
+            "title": "Lorem",
+        }
+        self.temp.api_post = MagicMock(return_value={"status_code": 408}, side_effect=ValueError)
+
+        assert_that(self.temp.api_post).raises(ValueError).when_called_with(post_todo)
+
     def tearDown(self):
         self.temp = None
 
