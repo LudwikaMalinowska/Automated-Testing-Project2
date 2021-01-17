126ed35ab0ee470d94e3698dfa00e29725d4d6a8
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 20:16:47 +0100

    Monkeypatching - GET - api_get_by_id - test sprawdzajacy czy dane zwracane przez metode api_get_by_id zgadzaja sie z oczekiwanymi danymi

--- tests/Monkeypatching/test_Api_monkeypatching_api_get_by_id.py
+++ tests/Monkeypatching/test_Api_monkeypatching_api_get_by_id.py
@@ -1,10 +1,12 @@
 import unittest
 
 import requests
+from assertpy import assert_that
 from requests.exceptions import Timeout
 from unittest.mock import Mock, patch
 
 from src.Api import Api
+from src.todos import todos
 
 
 class TestApiMonkeyPatch(unittest.TestCase):
@@ -53,6 +55,19 @@ class TestApiMonkeyPatch(unittest.TestCase):
             mock_id.return_value = 1
             mock_api.api_get_by_id(mock_id)
             mock_api.api_get_by_id.assert_called_once_with(mock_id)
+
+    def test_method_api_get_by_id_assert_that_response_equal_to_expected_userId_1_id_1_completed_false(self):
+        with patch('src.Api.Api', autospec=True) as mock_api:
+            todo_id = 1
+            mock_api.api_get_by_id.return_value = {"data": todos[todo_id - 1], "status_code": 200}
+            response = mock_api.api_get_by_id(todo_id)
+            expected_todo = {
+                "userId": 1,
+                "id": 1,
+                "title": "delectus aut autem",
+                "completed": False
+            }
+            assert_that(response["data"]).is_equal_to(expected_todo)
 
     def test_method_api_get_by_id_assert_that_not_called_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
