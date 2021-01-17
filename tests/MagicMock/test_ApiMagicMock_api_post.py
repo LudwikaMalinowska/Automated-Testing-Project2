68e5b162d6f19bc4ba4894e80fb84006bbf4dad9
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 13:10:52 +0100

    MagicMock - POST - api_post - test sprawdzajacy ze odpowiedz zwracana przez metode api_post ma status code 200

--- tests/MagicMock/test_ApiMagicMock_api_post.py
+++ tests/MagicMock/test_ApiMagicMock_api_post.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock, patch
 from assertpy import assert_that
 from requests import Timeout
 
@@ -8,7 +8,23 @@ from tests.Mock.todos import todos
 
 
 class TestApi(unittest.TestCase):
-    ...
+
+    def setUp(self):
+        self.temp = Api()
+
+    def test_method_api_post_assert_that_response_has_status_code_200(self):
+        post_todo = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
+        self.temp.api_post = MagicMock(return_value={"data": post_todo, "status_code": 200})
+        response = self.temp.api_post(post_todo)
+
+        assert_that(response).has_status_code(200)
+
+    def tearDown(self):
+        self.temp = None
 
 
 if __name__ == '__main__':
