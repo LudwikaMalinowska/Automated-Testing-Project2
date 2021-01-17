a87a05012438e2a231c56ef0cfa6439359153c11
Ludwika Malinowska <malinowska.L@wp.pl>
2021-01-17 20:45:01 +0100

    Monkeypatching - POST - poprawki w pliku test_Api_monkeypatching_api_post.py

--- tests/Monkeypatching/test_Api_monkeypatching_api_post.py
+++ tests/Monkeypatching/test_Api_monkeypatching_api_post.py
@@ -12,7 +12,11 @@ class TestApiMonkeyPatch(unittest.TestCase):
     @patch('src.Api.Api', autospec=True)
     def test_method_api_post_raises_timeout(self, mock_class):
         mock_data = Mock()
-        mock_data.return_value = {"key": "value"}
+        mock_data.return_value = {
+            "userId": 1,
+            "title": "Lorem",
+            "completed": False
+        }
         mock_class.api_post.side_effect = Timeout
         with self.assertRaises(Timeout):
             mock_class.api_post(mock_data)
@@ -20,16 +24,28 @@ class TestApiMonkeyPatch(unittest.TestCase):
     def test_method_api_post_assert_that_called_once(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
-            mock_data.return_value = {"key": "value"}
+            mock_data.return_value = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
             mock_api.api_post(mock_data)
             mock_api.api_post.assert_called_once()
 
     def test_method_api_post_assert_that_called(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
-            mock_data.return_value = {"key": "value"}
+            mock_data.return_value = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
             mock_data2 = Mock()
-            mock_data2.return_value = {"key2": "value2"}
+            mock_data2.return_value = {
+                "userId": 2,
+                "title": "Lorem ipsum",
+                "completed": True
+            }
             mock_api.api_post(mock_data)
             mock_api.api_post(mock_data2)
             mock_api.api_post.assert_called()
@@ -37,7 +53,11 @@ class TestApiMonkeyPatch(unittest.TestCase):
     def test_method_api_post_assert_that_not_called(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
-            mock_data.return_value = {"key": "value"}
+            mock_data.return_value = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
             mock_api.api_post.assert_not_called()
 
     def test_method_api_post_assert_that_called_with_mock_data_userId_1_title_Lorem(self):
@@ -65,7 +85,11 @@ class TestApiMonkeyPatch(unittest.TestCase):
     def test_method_api_post_assert_that_not_called_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
-            mock_data.return_value = {"key": "value"}
+            mock_data.return_value = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
             mock_api.api_post(mock_data)
             with self.assertRaises(AssertionError):
                 mock_api.api_post.assert_not_called()
@@ -73,9 +97,17 @@ class TestApiMonkeyPatch(unittest.TestCase):
     def test_method_api_post_assert_that_called_once_exception(self):
         with patch('src.Api.Api', autospec=True) as mock_api:
             mock_data = Mock()
-            mock_data.return_value = {"key": "value"}
+            mock_data.return_value = {
+                "userId": 1,
+                "title": "Lorem",
+                "completed": False
+            }
             mock_data2 = Mock()
-            mock_data2.return_value = {"key2": "value2"}
+            mock_data2.return_value = {
+                "userId": 2,
+                "title": "Lorem ipsum",
+                "completed": True
+            }
             mock_api.api_post(mock_data)
             mock_api.api_post(mock_data2)
             with self.assertRaises(AssertionError):
